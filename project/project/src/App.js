ae93131238294a1f16c0aaa9db6f6b722981aace
LAB TIK <baraniko90@gmail.com>
2022-10-24 21:11:36 +0700

    project

---
+++ project/project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="">
+      
+    </div>
+  );
+}
+
+export default App;
